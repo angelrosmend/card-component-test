8a59e74f884439cd6be5dce9123159a1d6c4e6c9
angelrosmend <angelrosmend@gmail.com>
2023-01-17 15:20:27 +0100

    (feat/components): create product list components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { favorite_off, favorite_on, star_off, star_on } from './components/Icon_svg';
+import IconSVG from './components/Icon_svg/IconSVG';
+import ProductsList from './components/ProductList/ProductsList';
+import "./styles/main.scss"
 
 function App() {
+
   return (
-    <div>
-    </div>
+    <Fragment>
+      <ProductsList/>
+    </Fragment>
   );
 }
 
---
+++ src/components/ProductList/ProductsList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProductsList() {
+  return (
+    <div></div>
+  )
+}
+
+export default ProductsList
