4d21ff3ac3f6ffcba5d741fcbe58c263e035f871
angelrosmend <angelrosmend@gmail.com>
2023-01-17 18:58:34 +0100

    (feat/components): add text component

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+interface TextProps{
+  title: boolean,
+  subtitle: boolean,
+  className: string,
+  style: object,
+  children: string,
+}
+function Text(props: TextProps) {
+
+    const {children, title, subtitle, ...rest} = props
+     console.log(rest)
+    if(title) return <h1 {...rest}>{children}</h1>
+
+    if(subtitle) return <h1 {...rest}>{children}</h1>
+
+    return <p {...rest}>{children}</p>
+}
+
+export default Text
