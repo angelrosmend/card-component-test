89b0830f7eb17e43d9ef7eba36b8fe467dc904ff
angelrosmend <angelrosmend@gmail.com>
2023-01-18 23:46:05 +0100

    (feat/styles): apply app global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React, { Fragment } from 'react';
-import { favorite_off, favorite_on, star_off, star_on } from './components/Icon_svg';
-import IconSVG from './components/Icon_svg/IconSVG';
 import ProductsList from './components/ProductList/ProductsList';
-import "./styles/main.scss"
+import styles from "./styles/App.module.scss"
 
 function App() {
 
   return (
-    <Fragment>
+    <div className={styles.appContainer}>
       <ProductsList/>
-    </Fragment>
+    </div>
   );
 }
 
