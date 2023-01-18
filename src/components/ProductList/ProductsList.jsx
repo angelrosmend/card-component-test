2b73969817ef71d4e94667374b07e7e4eb83dd1e
angelrosmend <angelrosmend@gmail.com>
2023-01-18 22:43:46 +0100

    add sass dependencies

--- src/components/ProductList/ProductsList.jsx
+++ src/components/ProductList/ProductsList.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import styles from "./productList.module.scss"
 
 function ProductsList() {
+  console.log(styles)
   return (
-    <div></div>
+    <div className={styles.containerTest}>Hello</div>
   )
 }
 
