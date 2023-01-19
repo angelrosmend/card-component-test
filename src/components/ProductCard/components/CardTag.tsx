a16b67ba5df5a91f29d06b1a56f0a2a5f962e877
angelrosmend <angelrosmend@gmail.com>
2023-01-19 22:54:24 +0100

    (feat/components): create card tags component

---
+++ src/components/ProductCard/components/CardTag.tsx
@@ -0,0 +1,21 @@
+import Text from "../../Text/Text";
+import styles from "../styles/productCard.module.scss";
+import typography from "../../../styles/_typography.module.scss";
+import { cardTagProps } from "../types";
+
+function CardTag({ label, content, mode }: cardTagProps) {
+  return (
+    <div
+      className={`${
+        mode === "primary" ? styles.cardTagPrimary : styles.cardTagSecondary
+      }`}
+    >
+      <Text label className={typography.cardTags}>
+        {label}
+      </Text>
+      <Text className={typography.cardTags}>{content}</Text>
+    </div>
+  );
+}
+
+export default CardTag;
