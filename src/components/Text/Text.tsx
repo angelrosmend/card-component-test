069050e126a71961da1157a14a724210b7171019
angelrosmend <angelrosmend@gmail.com>
2023-01-21 00:16:50 +0100

    (feat/component): add label to text component

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -2,19 +2,22 @@ import React from 'react'
 
 
 interface TextProps{
-  title: boolean,
-  subtitle: boolean,
-  className: string,
-  style: object,
-  children: string,
+  title?: boolean,
+  subtitle?: boolean,
+  label?: boolean,
+  className?: string,
+  style?: object,
+  children: string | number,
 }
 function Text(props: TextProps) {
 
-    const {children, title, subtitle, ...rest} = props
+    const {children, title, subtitle, label, ...rest} = props
     
     if(title) return <h1 {...rest}>{children}</h1>
 
-    if(subtitle) return <h1 {...rest}>{children}</h1>
+    if(subtitle) return <h3 {...rest}>{children}</h3>
+
+    if(label) return <label {...rest}>{children}</label>
 
     return <p {...rest}>{children}</p>
 }
