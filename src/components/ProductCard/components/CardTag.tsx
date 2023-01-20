e3277dc84e1575798c66d101afbfd482003fc10d
angelrosmend <angelrosmend@gmail.com>
2023-01-21 00:13:47 +0100

    (feat/styles): add styles to tag

--- src/components/ProductCard/components/CardTag.tsx
+++ src/components/ProductCard/components/CardTag.tsx
@@ -3,9 +3,10 @@ import styles from "../styles/productCard.module.scss";
 import typography from "../../../styles/_typography.module.scss";
 import { cardTagProps } from "../types";
 
-function CardTag({ label, content, mode }: cardTagProps) {
+function CardTag({ label, content, mode, style }: cardTagProps) {
   return (
     <div
+      style={style}
       className={`${
         mode === "primary" ? styles.cardTagPrimary : styles.cardTagSecondary
       }`}
