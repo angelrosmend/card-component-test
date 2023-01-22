9c1de5bb831888f1d1ed7a9144fde293eaf6c104
angelrosmend <angelrosmend@gmail.com>
2023-01-22 22:51:41 +0100

    (feat/state): add useFetch hook

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,29 +1,24 @@
 import axios from 'axios'
 import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import { productsState } from '../types'
 
 interface useFetchProps {
     url: string,
-    timeout?: number
+    timeout?: number,
+    initialState: productsState,
 }
 
-function useFetch({url, timeout}: useFetchProps) {
-    const initialState = useMemo(()=>{
-        return {
-                data: {},
-                loading: false,
-                error: false,
-                errorMessage: null
-            }
-    },[])
+function useFetch({url, timeout, initialState}: useFetchProps) {
+
     const [state, setState] = useState(initialState)
 
     const getData = useCallback(async() =>{
         setState((prevState)=> ({...prevState, loading: true}))
         try{
-         let response = await axios.get(url)
+         let response = await axios.get(url, {timeout})
          setState((prevState)=> ({...prevState, data: response.data, error: false, errorMessage: null}))
         }catch(err:any){
-         setState((prevState)=> ({...prevState, error: true, errorMessage: err.message, data: {}}))
+         setState((prevState)=> ({...prevState, error: true, errorMessage: err.message, data: []}))
         }finally{
           setState((prevstate) => ({...prevstate, loading: false}))
         }
@@ -32,7 +27,7 @@ function useFetch({url, timeout}: useFetchProps) {
     useEffect(()=>{
         getData()
     },[])
-    return [state, setState]
+    return state
 }
 
 export default useFetch
