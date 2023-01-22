c5f138399eb3687aa6c273d5396f2eb4520df0ed
angelrosmend <angelrosmend@gmail.com>
2023-01-22 22:55:33 +0100

    (feat/state): use context state to get items

--- src/pages/ProductList/ProductsList.jsx
+++ src/pages/ProductList/ProductsList.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import useFetch from '../../components/hooks/useFetch'
-import ProductCard from '../../components/ProductCard/ProductCard'
+import { Triangle } from 'react-loader-spinner'
+import { useProductsContext } from '../../context/ProductsContext'
 import styles from "./productList.module.scss"
+import colors from "../../styles/_colors.module.scss"
+import Text from '../../components/Text/Text'
+import ProductCard from '../../components/ProductCard/ProductCard'
 
 function ProductsList() {
-  const [data] = useFetch({url:"https://my-json-server.typicode.com/angelrosmend/card-component-test/products"})
+  const {data: products, loading, error} = useProductsContext()
+  if(loading) return <Triangle color={colors.mainBlack}/>
 
-  console.log(data)
+  if(!products || products.length < 1) return <Text title>No products found</Text>
+
+  if(error) return <Text subtitle> Ooops, something went wrong!</Text>
+
   return (
     <div className={styles.listContainer}>
-
+      {products.map((item)=>{
+         return <ProductCard key={item.id} item={item}/>
+      })}
     </div>
   )
 }
