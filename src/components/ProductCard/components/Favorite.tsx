72f30f446144b3b4d215f9d48deefc3803daf736
angelrosmend <angelrosmend@gmail.com>
2023-01-23 14:19:20 +0100

    (feat/components): add favorites local state

--- src/components/ProductCard/components/Favorite.tsx
+++ src/components/ProductCard/components/Favorite.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { favorite_off, favorite_on } from "../../Icon_svg";
 import IconSVG from "../../Icon_svg/IconSVG";
 import { favoriteProps } from "../types";
 import styles from "../styles/productCard.module.scss";
 
 function Favorite({ isFavorite }: favoriteProps) {
+  const [favorite, setFavorite] = useState(isFavorite)
+
   return (
     <IconSVG
       className={styles.favoriteBtn}
-      iconProps={isFavorite ? favorite_on : favorite_off}
+      iconProps={favorite ? favorite_on : favorite_off}
+      onClick={()=> setFavorite((prev)=> !prev)}
     />
   );
 }
