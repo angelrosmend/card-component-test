d42e1c47b9ab9e8876f8b8d4e8f25d367be5e189
angelrosmend <angelrosmend@gmail.com>
2023-01-23 23:32:51 +0100

    (feat/stories): install & add stories

---
+++ src/components/ProductCard/helpers/cardProduct.mock.ts
@@ -0,0 +1,24 @@
+import { productProps } from "../../../types";
+
+export const cardProductMock: productProps = {
+    id: 2,
+     image: [
+                "https://m.media-amazon.com/images/I/51KH9QAODZL._AC_SX425_.jpg",
+                "https://m.media-amazon.com/images/I/51r5DxTQsZL._AC_SL1000_.jpg",
+                "https://m.media-amazon.com/images/I/41mOGNxWQhL._AC_SL1000_.jpg"
+              ],
+    title:"Adventure Neo 3.1 EQ",
+    brand: "Cannondale",
+    year: 2022,
+    condition: "nuovo",
+    favorite: false,
+    description: "Una bici elettrica perfetta per la città, le piste ciclabili ed i sentieri di campagna.",
+    price: 3500,
+    old_price: null,
+    taglia: ["S", "M", "L", "XL"],
+    colori_qty: "3",
+    stars: 4,
+    rating: 100000,
+    currency: "€",
+    eBike: "corsa"
+   }
